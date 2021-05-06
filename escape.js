2698cf327c7a8c79151c3405387e78f4bdfac599
Alex Chen <achen115@ucr.edu>
2021-05-06 03:38:49 -0700

    slides

--- escape.js
+++ escape.js
@@ -4,7 +4,7 @@ function setHTML(str) {
 		//.replaceAll('<', '&lt;')
 		//.replaceAll('>', '&gt;')
 		//.replaceAll('"', '&quot;').trim();
-	console.log(str);
+	//console.log(str);
 	let e = document.currentScript.parentElement
 	e.innerHTML = '';
 	e.innerText = str;
--- slides.js
+++ slides.js
@@ -55,27 +55,93 @@ function navigateSlide(index) {
 	let href = location.href;
 	let q = href.indexOf("?");
 	let h = href.indexOf("#");
+	let args = null;
 	if (q > -1) {
-
 		let str = h > -1 ? href.substring(q + 1, h) : href.substring(q + 1);
-		let args = new URLSearchParams(str);
-		if (args.has('speakerNotes')) {
-			let speakerNotes = s.querySelector('.speakerNotes');
-			if (speakerNotes) {
-				let speakerDiv = document.body.querySelector('.speakerNotesBox');
-				if (!speakerDiv) {
-					speakerDiv = document.createElement('div');
-					speakerDiv.className = 'speakerNotesBox';
-					slide.parent().append(speakerDiv)
-				}
-				speakerDiv.innerHTML = speakerNotes.innerHTML;
-			} else {
-				let speakerDiv = document.body.querySelector('.speakerNotesBox');
-				if (speakerDiv)
-					speakerDiv.remove()
-			}
+		args = new URLSearchParams(str);
+	}
+	let speakerNotes = s.querySelector('.speakerNotes');
+	let speakerDiv = document.body.querySelector('#speakerNotesBox');
+	if (args && args.has('speakerNotes') && speakerNotes) {
+		console.log('speaker notes');
+		if (!speakerDiv) {
+			speakerDiv = document.createElement('div');
+			speakerDiv.id = 'speakerNotesBox';
+
+
+			slide.parent().append(speakerDiv)
+		}
+		speakerDiv.innerHTML = speakerNotes.innerHTML;
+	} else {
+		console.log('no speaker notes');
+		if(speakerDiv) {
+			speakerDiv.remove()
 		}
 	}
+
+	for(let n of document.querySelectorAll('.speakerNotes')) {
+		console.log(n.parentElement);
+		console.log('deleting speaker notes');
+		console.log(n.outerHTML);
+		n.remove();
+	}
+	if(args && args.has('console')) {
+		let d = document.createElement('div');
+		d.id = 'consoleBox';
+
+		let ta = document.createElement('textarea');
+		ta.style = `width:100%; height:320px; font-size:24px;`;
+		d.append(ta);
+
+		function run() {
+			
+
+			let s = document.createElement('script');
+			s.innerText = ta.value;
+			d.append(s);
+
+			let h = document.createElement('pre');
+			h.style = 'margin-top:32px;';
+			h.innerText = ta.value;
+			history.prepend(h);
+
+			h.onclick = () => ta.value = h.innerText;
+
+			ta.value = '';
+		}
+
+		let r = document.createElement('button');
+		r.innerText = 'Run';
+		r.style=  'font-size:32px;'
+		r.onclick = run;
+		d.append(r);
+
+		let history = document.createElement('div');
+		d.append(history);
+
+		slide.parent().append(d);
+	}
+
+	for(let n of document.querySelectorAll('#sourceBox')) {
+		console.log(n.parentElement);
+		console.log('deleting speaker notes');
+		console.log(n.outerHTML);
+		n.remove();
+	}
+	
+	if(args && args.has('source')) {
+		console.log('source');
+		let d = document.createElement('div');
+		d.id = 'sourceBox';
+		let pre = document.createElement('pre');
+		pre.innerText = 'Source';
+		for(let e of document.body.querySelectorAll("#slide *")) {
+			e.onclick = () => pre.innerText = e.outerHTML.replace(pre.innerHTML, 'Source');
+		}
+		d.append(pre);
+		slide.parent().append(d);
+	}
+
 
 	let number = document.getElementById('number');
 	if (number) {
@@ -108,11 +174,11 @@ function navigateSlide(index) {
 		let k = event.which;
 		console.log('keypress: ' + k)
 		switch (k) {
-			case 13: //Enter
-			case 39: //Right
+			//case 13: //Enter
+			case 34: //Page down
 				navigateUrl(index + 1);
 				break;
-			case 37: //Left
+			case 33: //Page up
 				navigateUrl(index - 1);
 				break;
 		}
